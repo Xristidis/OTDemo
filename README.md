9ed84a9e3fb021464f7b73527f73de5e2ade0816
Peter Christidis <peter.xristidis@gmail.com>
2020-07-02 21:31:39 -0400

    open table heroku demo

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# OTDemo
+# Open_Table_Api [React Redux App] 
+
+This is a full-stack app designed to consume the open-table API.
+
+[Live Link](http://178.128.231.249)
+
+I am using **React, Redux**, axios and AntDesign (for React Design Framework) on the front end and **Express** on the backend.
+
+To run this app:
+
+* `git clone` - Clone this repo***** add REPO
+* `cd into_directory` - Access Root Directory
+* `npm install` - Install All Dependencies
+* `npm start` - Launch Application Locally   
+
+*Make Sure You Have nodeJs Installed Locally*
+
---
+++ server.js
@@ -0,0 +1,86 @@
+const express = require('express');
+
+const app = express();
+const path = require('path');
+
+app.use(express.static(`${__dirname}/build`))
+
+app.get("/", (req, res) => {
+
+    res.sendFile(path.join(__dirname, "build", "index.html"))
+})
+
+app.get('/api', (req, res) => {
+    res.send(
+        {
+            "name": [{
+                "firstName": "Peter",
+                "lastName": "Christidis"
+            }],
+            "skills": [{
+                "front-end": [
+                    "HTML",
+                    {
+                        "CSS": [
+                            "Flexbox",
+                            "AntD",
+                            "Scss",
+                        ]
+                    },
+                    {
+                        "javaScript": [
+                            "ReactJs",
+                            "Redux",
+                            "JQuery",
+                            "NodeJS",
+                            "Socket.io",
+                            "ES6", {
+                                "testing": [
+                                    "Jest",
+                                    "Enzyme"
+                                ]
+                            }
+                        ]
+                    },
+                    "ReactJs",
+                    "Redux",
+                ],
+                "backEnd": [
+                    "NodeJs",
+                    "ExpressJs",
+                ],
+                "buildTools": [
+                    "Webpack",
+                    "Grunt",
+                    "Bower"
+                ],
+                "CMS": [
+                    "WordPress",
+                    "Squarespace"
+                ],
+                "cloudComputing": [
+                    "Heroku",
+                    "Netlify"
+                ],
+                "tools": [
+                    "Git",
+                    "Git Hub",
+                    "Visual Studio Code",
+                    "Jira",
+                    "Slack"
+                ]
+            }],
+            "interests": [
+                "Programming",
+                "Football (soccer)",
+                "Philosophy",
+                "Rock climbing",
+                "Board Games"
+            ]
+        }
+    )
+})
+
+app.listen(3000, () => {
+    console.log("port 3000 working!")
+})
